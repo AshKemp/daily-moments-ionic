2d1c17b80b2f0cb582fd60804c6b7a5cc6f5e87a
AshKemp <kmuraliashwin@gmail.com>
2023-09-22 22:56:22 +0530

    enable firebase storage service

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA4DIuKckArN7sIZ6zKX5tNXfdCd8nGNQ4",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = app.auth();
 export const firestore = firebase.firestore();
+export const storage = firebase.storage();
